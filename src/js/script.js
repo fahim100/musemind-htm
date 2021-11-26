e905284460d12ee87d9cf2d6ec49c0b4cd98fed7
fahim100 <fahimahmmed100@gmail.com>
2021-11-26 21:17:47 +0600

    Initialize commit.

---
+++ src/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    'use strict';
+
+    $('.clientLogo').liMarquee({
+        drag: false,
+        hoverstop:false
+    });
+    $('.designHeadingTitle').liMarquee({
+        drag: false,
+        hoverstop:false
+    });
+    $('.designHeadingTitle2').liMarquee({
+        drag: false,
+        hoverstop:false
+    });
+    // $('.designContent').liMarquee({
+    //     drag: false,
+    //     hoverstop:false
+    // });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let sections2 = gsap.utils.toArray(".heroProjectItem");
+
+    let scrollTween2 = gsap.to(sections2, {
+        xPercent: -100 * (sections2.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".hero",
+            pin: true,
+            scrub: 1,
+            end: () => "+=" + document.querySelector(".hero").offsetWidth
+        }
+    });
+
+    let sections = gsap.utils.toArray(".designContentItem");
+
+    let scrollTween = gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".designContent",
+            pin: true,
+            scrub: 1,
+            end: () => "+=" + document.querySelector(".designContent").offsetWidth
+        }
+    });
+
+
+
+    // sections.forEach((sct, i) => {
+    
+    // ScrollTrigger.create({
+    //     trigger: sct,
+    //     containerAnimation: scrollTween,
+    //     start: 'left 25%',
+    //     end: 'right 75%',
+    //     toggleClass: {targets: sct, className: "active"},
+
+    // })
+    
+    // });
+
+    
+});
